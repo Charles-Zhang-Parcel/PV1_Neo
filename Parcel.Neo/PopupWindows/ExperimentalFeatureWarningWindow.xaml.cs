b7751c2428b8e5a05e65b64ae85e450b4602ad98
Charles Zhang <szinubuntu@gmail.com>
2024-07-04 11:49:11 -0400

    Create ExperimentalFeatureWarningWindow

---
+++ Parcel.Neo/PopupWindows/ExperimentalFeatureWarningWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Parcel.Neo.PopupWindows
+{
+    public partial class ExperimentalFeatureWarningWindow : Window
+    {
+        #region Construction
+        public ExperimentalFeatureWarningWindow(string message)
+        {
+            InitializeComponent();
+
+            ContentTextBlock.Text = message;
+        }
+        #endregion
+
+        #region Events
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+        private void ContinueButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+        #endregion
+    }
+}
